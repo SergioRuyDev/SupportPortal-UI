e54c41af8eef25903b6c1df70ae25c1c699ac52b
SergioRuyDev <sergioruyenator@gmail.com>
2021-04-18 13:23:12 +0400

    Ready for deploy.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'PROD URL'
+  apiUrl: 'http://ruydev.us-east-2.elasticbeanstalk.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081'
+  apiUrl: 'http://ruydev.us-east-2.elasticbeanstalk.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
